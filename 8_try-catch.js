6bc659153b14c4b46843ffd301ac07090ad53ecf
Arfanur <arfanur03@gmail.com>
2022-04-02 12:36:11 +0600

    8_try-catch _29-8 Different types...

---
+++ 8_try-catch.js
@@ -0,0 +1,20 @@
+const myName = 'ami';
+// const student=undefined;
+// console.log(student.name);
+// console.log(student());
+// for(let i=0; i<10 i++){
+
+try {
+    myName = 'tumi';
+}
+catch (error) {
+    console.log('getting error', error);
+}
+finally{
+}
+console.log(100);
+console.log(myName);
+
+// }
+// name = 'tumi';
+// console.log(name);
