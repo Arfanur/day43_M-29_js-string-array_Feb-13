d9ba51644f144c2eab82c7b0733269e61b4972c6
Arfanur <arfanur03@gmail.com>
2022-04-04 10:19:08 +0600

    10_quiz

---
+++ 10_quiz.js
@@ -0,0 +1,11 @@
+// 3.
+const str1 = 'Bangladesh is a beautiful country.';
+console.log(str1.endsWith("."));
+
+// 8.
+// for(let i==0;i<10;i++){
+// }
+
+// 15.
+const array = ["121", "12", "1", "112", "111"];
+console.log(array.sort());
